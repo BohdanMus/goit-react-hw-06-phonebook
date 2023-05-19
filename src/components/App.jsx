bb4d46db8ea904d8e1c60d2d580dfc0f20676dd6
BohdanMus <bogdanmusienko@gmail.com>
2023-05-19 21:50:27 +0300

    add file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,123 @@
+import { useState, useEffect } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { GlobalStyle } from './GlobalStyle';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+
 export const App = () => {
+  const [contacts, setContacts] = useState(
+    () =>
+      JSON.parse(window.localStorage.getItem('contacts')) ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+  );
+
+  const [filter, setFilter] = useState('');
+
+  const onChangeInput = e => {
+    setFilter(e.currentTarget.value);
+  };
+  const filterNew = () => {
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+    return filteredContacts;
+  };
+
+  const addContact = newContact => {
+    if (
+      contacts.some(
+        contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+      )
+    ) {
+      alert(`${newContact.name} is already in contacts`);
+    } else {
+      setContacts(prevState => [...prevState, newContact]);
+    }
+  };
+
+  const deleteContact = id => {
+    const deletedContactById = contacts.filter(contact => contact.id !== id);
+    setContacts(deletedContactById);
+  };
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <GlobalStyle />
+      <h1>Phonebook</h1>
+      <ContactForm onAdd={addContact} />
+      <h2>Contacts</h2>
+      <Filter filter={filter} onChangeInput={onChangeInput} />
+      <ContactList contacts={filterNew()} onDelete={deleteContact} />
     </div>
   );
 };
+
+// export class App extends Component {
+//   state = {
+//     contacts: [
+//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+//     ],
+//     filter: '',
+//   };
+
+//   componentDidMount() {
+//     const savedContacts = localStorage.getItem('contacts');
+//     if (savedContacts !== null) {
+//       this.setState({ contacts: JSON.parse(savedContacts) });
+//     } else {
+//       this.setState({ contacts: this.state.contacts });
+//     }
+//   }
+
+//   componentDidUpdate(_, prevState) {
+//     if (prevState.contacts !== this.state.contacts) {
+//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+//     }
+//   }
+
+//   onChangeInput = e => {
+//     const { name, value } = e.currentTarget;
+//     this.setState({ [name]: value });
+//   };
+
+//   filter = () => {
+//     const { contacts, filter } = this.state;
+//     const filteredContacts = contacts.filter(contact =>
+//       contact.name.toLowerCase().includes(filter.toLowerCase())
+//     );
+//     return filteredContacts;
+//   };
+
+//   addContact = newContact => {
+//     if (
+//       this.state.contacts.some(
+//         contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+//       )
+//     ) {
+//       alert(`${newContact.name} is already in contacts`);
+//     } else {
+//       this.setState(prevState => ({
+//         contacts: [...prevState.contacts, newContact],
+//       }));
+//     }
+//   };
+
+//   deleteContact = id => {
+//     const { contacts } = this.state;
+//     const deletedContactById = contacts.filter(contact => contact.id !== id);
+//     this.setState({ contacts: deletedContactById });
+//   };
+
+//   render() {}
+// }
